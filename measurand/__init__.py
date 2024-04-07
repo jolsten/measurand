7e7b7dc04a2047d43ddf6d6440d88c12182d409d
jolsten <jolsten@gmail.com>
2024-04-07 10:01:25 -0400

    initial commit

---
+++ measurand/__init__.py
@@ -0,0 +1,4 @@
+from measurand.measurand import Measurand, make_measurand
+from measurand.parameter import Parameter, make_parameter
+
+__all__ = ["make_measurand", "Measurand", "make_parameter", "Parameter"]
